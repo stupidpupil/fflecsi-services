3cfd3cd9eb31897fcefb9ea604a93c11f1ec1e65
Adam Watkins <adam@stupidpupil.co.uk>
2023-12-09 10:19:44 +0000

    Cope with 404 errors for locations

--- R/get_details_and_timetables_for_fflecsi_locations.R
+++ R/get_details_and_timetables_for_fflecsi_locations.R
@@ -5,12 +5,13 @@ get_details_and_timetables_for_fflecsi_locations <- function(){
   details <- tibble::tibble()
 
   for (url in urls) {
+    try({
+      d <- get_details_and_timetable_for_fflecsi_location_url(url)
 
-    d <- get_details_and_timetable_for_fflecsi_location_url(url)
+      timetables <- timetables |> bind_rows(d$timetables)
+      details <- details |> bind_rows(d$details)
 
-    timetables <- timetables |> bind_rows(d$timetables)
-    details <- details |> bind_rows(d$details)
-
+    })
   }
 
   return(list(details = details, timetables = timetables))
